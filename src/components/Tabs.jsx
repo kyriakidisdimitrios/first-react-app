f490471a8a8263995ce784d56123169139e8055b
kyriakidisdimitrios <kyriakidis.demetrios@gmail.com>
2024-06-02 20:28:43 +0100

    feat: Added dummy tic-tac-toe elements

--- src/components/Tabs.jsx
+++
@@ -1,9 +0,0 @@
-export default function Tabs({ children, buttons, ButtonsContainer = 'menu' }) { //anti na baloyme to menu sto return tou Tabs.jsx
-  // const ButtonsContainer = buttonsContainer;
-  return (
-    <>
-      <ButtonsContainer>{buttons}</ButtonsContainer>
-      {children}
-    </>
-  );
-}

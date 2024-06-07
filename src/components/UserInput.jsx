957d674e76e5f50f1fb71bde592cc7fe15de8098
kyriakidisdimitrios <kyriakidis.demetrios@gmail.com>
2024-06-08 00:17:28 +0100

    feat: step 2

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -22,21 +22,25 @@ export default function UserInput(){
         <div className = "input-group">
             <p>
                 <label>Initial Investment</label>
-                <input type="number" required/>
+                {/*<input type="number" required onChange = {handleChange}/> se auton edw 8a estelne mono ena stoixeio automata apo thn react, anti autou 8a kanoume anonymous function*/}
+                {/*<input type="number" required onChange = {() => handleChange('initialInvestment',)}/> mesa sthn anonymous bazw thn event*/}
+                {/*thn value ={} thn valame gia na epistrefei h timh apo thn handleChange*/}
+                <input type="number" value ={userInput.initialInvestment} required onChange = {(event) => handleChange('initialInvestment', event.target.value)}/>
             </p>
             <p>
                 <label>Annual Investment</label>
-                <input type="number" required/>
+                <input type="number" value ={userInput.annualInvestment} required onChange = {(event) => handleChange('annualInvestment', event.target.value)}/>
             </p>
         </div>
         <div className = "input-group">
             <p>
                 <label>Expected Return</label>
-                <input type="number" required/>
-            </p>
+                <input type="number" value ={userInput.expectedReturn} required onChange = {(event) => handleChange('expectedReturn', event.target.value)}/>
+                    </p>
             <p>
                 <label>Duration</label>
-                <input type="number" required/>
+                <input type="number" value ={userInput.duration} required onChange = {(event) => handleChange('duration', event.target.value)}/>
+
             </p>
         </div>
     </section>);
